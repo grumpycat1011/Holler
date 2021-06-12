227fe698161e8e5b67d5d1cec717a5efadfc31ba
Siddhesh <siddhesh.a.borse@gmail.com>
2021-06-12 15:19:58 +0530

    Signup with firebase Implemented

--- app/src/main/java/com/example/holler/LoginActivity.java
+++ app/src/main/java/com/example/holler/LoginActivity.java
@@ -2,13 +2,30 @@ package com.example.holler;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
-
+    private EditText Lemail, Lpassword;
+    private Button Login, gotosignup;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        Lemail = findViewById(R.id.Lemail);
+        Lpassword = findViewById(R.id.Lpassword);
+        Login = findViewById(R.id.Login);
+        gotosignup = findViewById(R.id.button2);
+
+        gotosignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(),SignupActivity.class));
+            }
+        });
     }
 }
--- app/src/main/java/com/example/holler/SignupActivity.java
+++ app/src/main/java/com/example/holler/SignupActivity.java
@@ -1,14 +1,84 @@
 package com.example.holler;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class SignupActivity extends AppCompatActivity {
-
+    private EditText Rname, Ruserid, Remail, Rpass, Rconpass;
+    private Button signup;
+    FirebaseAuth fAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+        Rname = findViewById(R.id.Rname);
+        Ruserid = findViewById(R.id.Ruserid);
+        Remail = findViewById(R.id.Remail);
+        Rpass = findViewById(R.id.Rpass);
+        Rconpass= findViewById(R.id.Rconpass);
+        signup = findViewById(R.id.SignupB);
+        fAuth = FirebaseAuth.getInstance();
+
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String fullname = Rname.getText().toString();
+                String userid = Ruserid.getText().toString();
+                String useremail = Remail.getText().toString();
+                String password = Rpass.getText().toString();
+                String confirmpass = Rconpass.getText().toString();
+
+                if (fullname.isEmpty()){
+                    Rname.setError("Name is a mandatory field!");
+                }
+                if (userid.isEmpty()){
+                    Ruserid.setError("Username is a mandatory field!");
+                }
+                if (useremail.isEmpty()){
+                    Remail.setError("Email is a mandatory field!");
+                }
+                if (password.isEmpty()){//I am aware of the typo here
+                    Rpass.setError("Password is a mandatory field!");
+                }
+                if (confirmpass.isEmpty()){
+                    Rconpass.setError("Confirm password is a mandatory field!");
+                }
+                if(!password.equals(confirmpass)){
+                    Rconpass.setError("The passwords do not match");
+                }
+                fAuth.createUserWithEmailAndPassword(useremail,password).addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                    @Override
+                    public void onSuccess(AuthResult authResult) {
+                        startActivity(new Intent(getApplicationContext(),LoginActivity.class));
+                        Toast.makeText(SignupActivity.this, "Great success", Toast.LENGTH_SHORT).show();//I did this as a meme from borat change this later
+                        finish();
+
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull @org.jetbrains.annotations.NotNull Exception e) {
+                        Toast.makeText(SignupActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+
+
+
+
+
+            }
+        });
     }
 }
